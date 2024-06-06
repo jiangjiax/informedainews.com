611c6bcc2260f526a757bfc0b4d8e8a622d1a60d
jiangjiax <2921238406@qq.com>
2024-06-06 23:23:20 +0800

    social-card.jpeg

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social-card.jpeg',
       navbar: {
         title: 'Informed AI News',
         logo: {
