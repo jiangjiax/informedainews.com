d0cd25a2fde9ea579b329b54f4846373f8a07bb3
jiangjiax <2921238406@qq.com>
2024-06-12 16:43:35 +0800

    footer

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -27,14 +27,22 @@ export default function FooterWrapper(props) {
                 </svg>
               </a>
             </li>
-          </ul>
-        </div>
-        <div className="col footer__col">
-          <div className="footer__title">More</div>
-          <ul className="footer__items clean-list">
             <li className="footer__item">
               <a href="https://github.com/jiangjiax/informedainews.com" target="_blank" rel="noopener noreferrer" className="footer__link-item">
                 GitHub
+                <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" className={styles.icon}>
+                  <path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path>
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col footer__col">
+          <div className="footer__title">Friendship Link</div>
+          <ul className="footer__items clean-list">
+            <li className="footer__item">
+              <a href="https://woy.ai/" title="Woy AI Tools Directory" target="_blank" rel="noopener noreferrer" className="footer__link-item">
+                Woy AI
                 <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" className={styles.icon}>
                   <path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path>
                 </svg>
