c025ecf81568698d459b66def3513ae5bcc632ea
jiangjiax <2921238406@qq.com>
2024-06-13 19:55:29 +0800

    change style

--- src/components/ListGrid/index.js
+++ src/components/ListGrid/index.js
@@ -4,7 +4,7 @@ import Translate from '@docusaurus/Translate';
 
 export default function ListLink({ children }) {
     return (
-        <section className="text-gray-600 body-font overflow-hidden">
+        <section className="text-gray-600 body-font overflow-hidden pt-2">
             <h2 className="text-center text-3xl font-bold text-gray-900 dark:text-gray-100 pt-3"><Translate>News</Translate></h2>
             <p className="text-center text-gray-600 dark:text-gray-400"><Translate>news selected from the past 3 days</Translate></p>
             <div className='pl-5 pr-5'>
