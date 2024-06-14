d4bf2eb976e59808abacf7ee14e96fc3212a420a
jiangjiax <2921238406@qq.com>
2024-06-14 11:00:06 +0800

    latest 20240614

--- src/components/Latest/index.js
+++ src/components/Latest/index.js
@@ -11,17 +11,37 @@ const LatestHref = translate({
 
 const FeatureList = [
   {
-    title: <Translate id="Latest.title1">World News Daily</Translate>,
-    date: '2024-06-13',
-    href: '/docs/world-news-daily/1-2024-06-13',
+    title: <Translate id="Latest.title1">Tech Enthusiast Weekly</Translate>,
+    date: '2024-06-14',
+    href: '/docs/tech-enthusiast-weekly/2-2024-06-14',
     description: (
       <>
-        <Translate id="Latest.description4">UN Report Accuses Both Hamas and Israel of War Crimes</Translate>
+        <Translate id="Latest.description1">ASML Unveils Next-Generation 'Hyper-NA' EUV Technology Roadmap</Translate>
       </>
     ),
   },
   {
     title: <Translate id="Latest.title2">World News Daily</Translate>,
+    date: '2024-06-14',
+    href: '/docs/world-news-daily/1-2024-06-14',
+    description: (
+      <>
+        <Translate id="Latest.description2">ASML Unveils Next-Generation 'Hyper-NA' EUV Technology Roadmap</Translate>
+      </>
+    ),
+  },
+  {
+    title: <Translate id="Latest.title3">World News Daily</Translate>,
+    date: '2024-06-13',
+    href: '/docs/world-news-daily/1-2024-06-13',
+    description: (
+      <>
+        <Translate id="Latest.description3">UN Report Accuses Both Hamas and Israel of War Crimes</Translate>
+      </>
+    ),
+  },
+  {
+    title: <Translate id="Latest.title4">World News Daily</Translate>,
     date: '2024-06-12',
     href: '/docs/world-news-daily/1-2024-06-12',
     description: (
@@ -31,65 +51,45 @@ const FeatureList = [
     ),
   },
   {
-    title: <Translate id="Latest.title3">World News Daily</Translate>,
+    title: <Translate id="Latest.title5">World News Daily</Translate>,
     date: '2024-06-11',
     href: '/docs/world-news-daily/1-2024-06-11',
     description: (
       <>
-        <Translate id="Latest.description4">Trump Faces Probation Interview Amid Legal Battle Over Hush Money Conviction</Translate>
+        <Translate id="Latest.description5">Trump Faces Probation Interview Amid Legal Battle Over Hush Money Conviction</Translate>
       </>
     ),
   },
   {
-    title: <Translate id="Latest.title4">World News Daily</Translate>,
+    title: <Translate id="Latest.title6">World News Daily</Translate>,
     date: '2024-06-10',
     href: '/docs/world-news-daily/1-2024-06-10',
     description: (
       <>
-        <Translate id="Latest.description4">Final Day of European Parliament Elections: Voters Shape the Continent's Future</Translate>
+        <Translate id="Latest.description6">Final Day of European Parliament Elections: Voters Shape the Continent's Future</Translate>
       </>
     ),
   },
   {
-    title: <Translate id="Latest.title5">Tech Enthusiast Weekly</Translate>,
+    title: <Translate id="Latest.title7">Tech Enthusiast Weekly</Translate>,
     href: '/docs/tech-enthusiast-weekly/2-2024-06-10',
     date: '2024-06-10',
     description: (
       <>
-        <Translate id="Latest.description5">5G Commercialization in China: Economic Impact and Network Expansion</Translate>
+        <Translate id="Latest.description7">5G Commercialization in China: Economic Impact and Network Expansion</Translate>
       </>
     ),
   },
   {
-    title: <Translate id="Latest.title6">AI Enthusiast Weekly</Translate>,
+    title: <Translate id="Latest.title8">AI Enthusiast Weekly</Translate>,
     href: '/docs/ai-enthusiast-daily/3-2024-06-10',
     date: '2024-06-10',
     description: (
       <>
-        <Translate id="Latest.description6">Apple's WWDC 2024 to Introduce AI Enhancements in iOS 18 and iPadOS 18</Translate>
+        <Translate id="Latest.description8">Apple's WWDC 2024 to Introduce AI Enhancements in iOS 18 and iPadOS 18</Translate>
       </>
     ),
   },
-  {
-    title: <Translate id="Latest.title7">World News Daily</Translate>,
-    href: '/docs/world-news-daily/1-2024-06-09',
-    date: '2024-06-09',
-    description: (
-      <>
-        <Translate id="Latest.description7">Top 5 AI Tools Revolutionizing Content Creation in 2024</Translate>
-      </>
-    ),
-  },
-  {
-    title: <Translate id="Latest.title8">Tech Enthusiast Weekly</Translate>,
-    href: '/docs/tech-enthusiast-weekly/2-2024-06-09',
-    date: '2024-06-09',
-    description: (
-      <>
-        <Translate id="Latest.description8">Top 5 AI Tools Revolutionizing Content Creation in 2024</Translate>
-      </>
-    ),
-  }
 ];
 
 function Feature({ title, href, description, date }) {
