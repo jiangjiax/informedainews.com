b7ca885d8c88a0dd6c92079598d937dd7e48f783
jiangjiax <2921238406@qq.com>
2024-07-19 22:40:17 +0800

    20240719

--- src/components/Latest/index.js
+++ src/components/Latest/index.js
@@ -11,6 +11,16 @@ const LatestHref = translate({
 
 const FeatureList = [
   {
+    title: <Translate id="Latest.title7">World News Daily</Translate>,
+    date: '2024-07-19',
+    href: '/docs/world-news-daily/1-2024-07-19',
+    description: (
+      <>
+        <Translate id="Latest.description7">Central Committee Press Conference: Deepening Reforms, Advancing Chinese-Style Modernization</Translate>
+      </>
+    ),
+  },
+  {
     title: <Translate id="Latest.title8">World News Daily</Translate>,
     date: '2024-07-17',
     href: '/docs/world-news-daily/1-2024-07-17',
@@ -77,16 +87,6 @@ const FeatureList = [
     description: (
       <>
         <Translate id="Latest.description6">NATO Leaders Gather in Washington to Celebrate 75 Years and Discuss Ukraine Aid</Translate>
-      </>
-    ),
-  },
-  {
-    title: <Translate id="Latest.title7">World News Daily</Translate>,
-    date: '2024-07-09',
-    href: '/docs/world-news-daily/1-2024-07-09',
-    description: (
-      <>
-        <Translate id="Latest.description7">French Political Instability due to Election Uncertainty</Translate>
       </>
     ),
   },
