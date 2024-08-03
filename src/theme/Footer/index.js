91b1b8a5aef2c0a1d5d3f64d371f62e94f40849e
jiangjiax <2921238406@qq.com>
2024-08-03 16:05:26 +0800

    newweb

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -49,8 +49,8 @@ export default function FooterWrapper(props) {
               </a>
             </li>
             <li className="footer__item">
-              <a href="https://chainedtogether.top/" title="Woy AI Tools Directory" target="_blank" rel="noopener noreferrer" className="footer__link-item">
-              Chained Together
+              <a href="https://blackmythwukong.wiki/" title="Black Myth WuKong Wiki" target="_blank" rel="noopener noreferrer" className="footer__link-item">
+              Black Myth WuKong Wiki
                 <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" className={styles.icon}>
                   <path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path>
                 </svg>
